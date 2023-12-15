ce106044d2bf13033bcc2accf62fe5dbff33a336
SingHuyLeang <leang6525@gmail.com>
2023-12-15 09:36:29 +0700

    Condition

---
+++ src/Introduct/Main.java
@@ -0,0 +1,51 @@
+package Introduct;
+
+/*
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x;
+        System.out.print("Input x = "); x = scan.nextInt();
+
+         if statement
+            if (condition){
+
+            }
+         if else statement
+            if (condition){
+
+            }else{
+
+            }
+         if else if else statement
+            if (condition){
+
+            }else if (condition){
+
+            }else{
+
+            }
+         ternary operator
+            x = 1 > 2 ? 1 : 2;
+         switch case statement
+            switch (expression){
+                case value ->{
+
+                }
+            }
+    }
+}
+*/
+/*
+
+    .next();
+
+    String
+    .next();
+    .nextLine();
+
+    condition / expression
+    1 > 2     / day = 3
+ */
